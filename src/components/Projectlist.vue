bc1cdddb00e24100aaa607cc05b87e88078e0cac
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-14 18:23:33 +0200

    fix

--- src/components/Projectlist.vue
+++ src/components/Projectlist.vue
@@ -20,10 +20,14 @@ export default {
     created() {
         axios
             .get('http://localhost:8000/api/projects', {
+                params: {
+                    page: this.last_page,
+                }
 
             })
             .then(response => {
                 this.ArrProjects = response.data.data;
+                this.nPages = response.data.last_page;
 
             });
     },
@@ -38,14 +42,15 @@ export default {
         <ProjectCard v-for="project in ArrProjects" :key="project.id" :project="project" />
     </div>
 
-    <!-- <nav aria-label="Page navigation example">
+    <nav>
         <ul class="pagination">
             <li class="page-item">
                 <a class="page-link" href="#" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </a>
             </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li v-for="Page in nPages" :key="Page" class="page-item" :class="{ active: page == activePage }"><a
+                    class="page-link" href="#">{{ page }}</a></li>
             <li class="page-item"><a class="page-link" href="#">2</a></li>
             <li class="page-item"><a class="page-link" href="#">3</a></li>
             <li class="page-item">
@@ -54,7 +59,7 @@ export default {
                 </a>
             </li>
         </ul>
-    </nav> -->
+    </nav>
 </template>
 
 <style lang="sass">

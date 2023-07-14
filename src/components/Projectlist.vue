cee185c0558c057c493e3fc5bb142c2c7c2695ae
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-14 15:46:12 +0200

    import axios with created

---
+++ src/components/Projectlist.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+            ArrProject: [],
+        }
+    },
+
+    created() {
+        // Facciamo la richiesta al server tramite axios
+        axios
+
+    },
+}
+
+
+
+
+</script>
+
+<template></template>
+
+<style lang="sass">
+
+</style>

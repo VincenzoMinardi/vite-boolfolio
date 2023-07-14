3011606450ef92ff8b5676c6e4c7cbf3e80ef962
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-14 17:42:10 +0200

    sistemation cod

--- src/components/Projectlist.vue
+++ src/components/Projectlist.vue
@@ -8,15 +8,25 @@ export default {
     },
     data() {
         return {
-            ArrProjects: []
+            ArrProjects: [],
+
         };
     },
+
+
+
+
+
     created() {
-        axios.get('http://localhost:8000/api/projects')
+        axios
+            .get('http://localhost:8000/api/projects', {
+
+            })
             .then(response => {
                 this.ArrProjects = response.data.data;
-            })
-    }
+
+            });
+    },
 };
 </script>
 
@@ -27,6 +37,24 @@ export default {
     <div class="container d-flex flex-wrap mt-3 gap-5">
         <ProjectCard v-for="project in ArrProjects" :key="project.id" :project="project" />
     </div>
+
+    <!-- <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        </ul>
+    </nav> -->
 </template>
 
 <style lang="sass">

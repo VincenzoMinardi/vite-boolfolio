a592ecaf29a8324d30eae301df89d0ab62e75eba
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-17 17:45:20 +0200

    add links information

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,36 @@
 <script></script>
 
 <template>
-    <div>
+    <div class="text-center">
+        <article>
+            <h1>I miei Social</h1>
+            <a href="https://github.com/VincenzoMinardi">github</a>
+            <a href="https://www.instagram.com/vincenzo_minardi99/">Instagram</a>
+            <a href="https://www.linkedin.com/in/vincenzo-minardi-820077279/">LinkedIn</a>
 
+
+
+
+
+            <footer>
+                <p>© 2023 Boolean</p>
+            </footer>
+        </article>
     </div>
 </template>
 
-<style></style>
+<style>
+article {
+    min-height: 100%;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+}
+
+footer {
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    background-color: #45a1ff;
+    color: #fff;
+}
+</style>
